efb44dc0204eb92317250eeaaadac58e3cca85a1
Murat Topallar <topallarm@outlook.com>
2021-02-27 18:07:14 +0300

    Controller Add yazıldı. Çalışır durumda

--- WebAPI/Controllers/CarImagesController.cs
+++ WebAPI/Controllers/CarImagesController.cs
@@ -26,74 +26,65 @@ namespace WebAPI.Controllers
             _carImageService = carImageService;
         }
 
-        private string CheckCarImageByCarId(int carId)
-        {
-            var imagesPerCar = _carImageService.GetListByCarId(carId);
-
-            if (imagesPerCar.Data.Count >= 5)
-            {
-                return imagesPerCar.Message;
-            }
-
-            return (5 - (imagesPerCar.Data.Count)).ToString() + " adet resim sisteme eklendi";
-        }
-        [HttpPost]
+       
+        [HttpPost("add")]
         //Postman'da post ederken Key olarak UploadedImage Value olarak resimler eklenecek. Çoklu eklemeye izin var. bir de carId değeri vermelisin.
         public string Add([FromForm] ImageForUpload imageForUploads, [FromForm] int carId)
         {
             var imagesPerCar = _carImageService.GetListByCarId(carId);
-            
+
             try
             {
-                if (imageForUploads.UploadedImage!=null)
+                if (imageForUploads.UploadedImage != null)
                 {
-                    
-                }
-                if (imageForUploads.UploadedImage.Count > 0)
-                {
-                    foreach (var imageForUpload in imageForUploads.UploadedImage)
+                    if (imageForUploads.UploadedImage.Count > 0)
                     {
-
-                        if (imagesPerCar.Data.Count >= 5)
+                        foreach (var imageForUpload in imageForUploads.UploadedImage)
                         {
-                            var result = _carImageService.Add(new CarImage { CarId = carId });
-                            return result.Message;
-                        }
 
-                        string imagePath = _webHostEnvironment.WebRootPath + @"\CarImages\";
-                        if (!Directory.Exists(imagePath))
-                        {
-                            Directory.CreateDirectory(imagePath);
-                        }
-
-                        var guIdName = Guid.NewGuid().ToString("N") + "_" + DateTime.Now.Second;
-                        var fileExtension = new System.IO.FileInfo(imageForUpload.FileName).Extension;
-
-
-                        if (imageForUpload.FileName == "CarRentalDefault" + fileExtension)
-                        {
-                            using (FileStream fileStream = System.IO.File.Create(imagePath + "CarRentalDefault" + fileExtension))
+                            if (imagesPerCar.Data.Count >= 5)
                             {
-                                imageForUpload.CopyTo(fileStream);
-                                fileStream.Flush();
-                                var carImageForDb = new CarImage { CarId = carId, ImagePath = imagePath + "CarRentalDefault" + fileExtension, Date = DateTime.Now };
-                                _carImageService.Add(carImageForDb);
+                                var result = _carImageService.Add(new CarImage { CarId = carId });
+                                return result.Message;
+                            }
+
+                            string imagePath = _webHostEnvironment.WebRootPath + @"\CarImages\";
+                            if (!Directory.Exists(imagePath))
+                            {
+                                Directory.CreateDirectory(imagePath);
+                            }
+
+                            var guIdName = Guid.NewGuid().ToString("N") + "_" + DateTime.Now.Second;
+                            var fileExtension = new System.IO.FileInfo(imageForUpload.FileName).Extension;
+
+
+                            if (imageForUpload.FileName == "CarRentalDefault" + fileExtension)
+                            {
+                                using (FileStream fileStream = System.IO.File.Create(imagePath + "CarRentalDefault" + fileExtension))
+                                {
+                                    imageForUpload.CopyTo(fileStream);
+                                    fileStream.Flush();
+                                    var carImageForDb = new CarImage { CarId = carId, ImagePath = imagePath + "CarRentalDefault" + fileExtension, Date = DateTime.Now };
+                                    _carImageService.Add(carImageForDb);
+                                }
+                            }
+
+                            else
+                            {
+                                using (FileStream fileStream = System.IO.File.Create(imagePath + guIdName + fileExtension))
+                                {
+                                    imageForUpload.CopyTo(fileStream);
+                                    fileStream.Flush();
+                                    var carImageForDb = new CarImage { CarId = carId, ImagePath = imagePath + guIdName + fileExtension, Date = DateTime.Now };
+                                    _carImageService.Add(carImageForDb);
+                                }
                             }
                         }
 
-                        else
-                        {
-                            using (FileStream fileStream = System.IO.File.Create(imagePath + guIdName + fileExtension))
-                            {
-                                imageForUpload.CopyTo(fileStream);
-                                fileStream.Flush();
-                                var carImageForDb = new CarImage { CarId = carId, ImagePath = imagePath + guIdName + fileExtension, Date = DateTime.Now };
-                                _carImageService.Add(carImageForDb);
-                            }
-                        }
                     }
                     return "İzin verilen sayıda resim projeye dahil edildi.";
                 }
+
                 else
                 {
                     return "Lütfen yüklenecek resmi seçin.";
@@ -106,6 +97,8 @@ namespace WebAPI.Controllers
             }
 
         }
+
+        [HttpPost("update")]
 
     }
 }
