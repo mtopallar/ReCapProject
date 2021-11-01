2ce72e96fc6d5d0c731271d57770cf19bf9bced1
Murat Topallar <topallarm@outlook.com>
2021-11-01 16:35:45 +0300

    Token geçerlilik süresinde hataya sebep olan kod düzeltildi.

--- Core/Utilities/Security/Jwt/JwtHelper.cs
+++ Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -16,17 +16,17 @@ namespace Core.Utilities.Security.Jwt
     {
         public IConfiguration Configuration { get; }
         private readonly TokenOptions _tokenOptions;
-        readonly DateTime _accessTokenExpiration;
+        private DateTime _accessTokenExpiration;
 
         public JwtHelper(IConfiguration configuration)
         {
             Configuration = configuration;
-            _tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
-            _accessTokenExpiration = DateTime.Now.AddMinutes(_tokenOptions.AccessTokenExpiration);
+            _tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();            
         }
 
         public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
         {
+            _accessTokenExpiration = DateTime.Now.AddMinutes(_tokenOptions.AccessTokenExpiration);
             var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
             var signingCredentials = SigningCredentialsHelper.CreateSigningCredentials(securityKey);
             var jwt = CreateJwtSecurityToken(_tokenOptions, user, signingCredentials, operationClaims);
